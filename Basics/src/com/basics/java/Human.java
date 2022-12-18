b57fe19f4e137da5c2e582b073b040884580488c
RisitaShee <114274128+RisitaShee@users.noreply.github.com>
2022-12-18 11:42:55 +0530

    Update Human.java

--- Basics/src/com/basics/java/Human.java
+++ Basics/src/com/basics/java/Human.java
@@ -2,13 +2,47 @@ package com.basics.java;
 
 public class Human {
 //prop
-String name;
+String name="risita";
 int age;
 int heightInInches;
 String hairColor;
 
 //behaviour
 public void speak() {
+	
+	System.out.println("abcd");
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public int getAge() {
+	return age;
+}
+
+public void setAge(int age) {
+	this.age = age;
+}
+
+public int getHeightInInches() {
+	return heightInInches;
+}
+
+public void setHeightInInches(int heightInInches) {
+	this.heightInInches = heightInInches;
+}
+
+public String getHairColor() {
+	return hairColor;
+}
+
+public void setHairColor(String hairColor) {
+	this.hairColor = hairColor;
 }
 }
 
