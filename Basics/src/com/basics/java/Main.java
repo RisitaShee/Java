bec12cf6f612e6fb4ea7e65b2427c32637971c2a
RisitaShee <114274128+RisitaShee@users.noreply.github.com>
2022-12-21 17:10:41 +0530

    commit

--- Basics/src/com/basics/java/Main.java
+++ Basics/src/com/basics/java/Main.java
@@ -2,8 +2,12 @@ package com.basics.java;
 
 public class Main {
 public static void main(String[] args) {
+	System.out.println("kolkata");
+	System.out.println("behala");
+	System.out.print("kolkata");
 int myVar;
-myVar = 100;
+myVar = 500;
+myVar = myVar + 10;
 System.out.println("value of myVar is: " + myVar);
 
 }
