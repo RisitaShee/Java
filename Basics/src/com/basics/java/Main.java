655051ff04c93598d972c03be725b82613f28a88
RisitaShee <114274128+RisitaShee@users.noreply.github.com>
2022-12-22 19:12:33 +0530

    Update Main.java

--- Basics/src/com/basics/java/Main.java
+++ Basics/src/com/basics/java/Main.java
@@ -2,10 +2,12 @@ package com.basics.java;
 
 public class Main {
 public static void main(String[] args) {
-System.out.println("kolkata");
-System.out.println("behala");
+System.out.print("kolkata\t");
+System.out.print("behala\t");
 
-
+int myVar;
+myVar = 100;
+System.out.println("value of myVar is: " +myVar);
 
 }
 	
