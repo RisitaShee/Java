5101127f3066d2d68e73af83039a7c0a7175da31
RisitaShee <114274128+RisitaShee@users.noreply.github.com>
2022-12-26 20:56:46 +0530

    commit

---
+++ Basics/src/com/basics/java/Earth.java
@@ -0,0 +1,15 @@
+package com.basics.java;
+
+public class Earth {
+	public static void main(String[] args) {
+		Human risita;
+		risita = new Human();
+		risita.name ="risita";
+		risita.age =24;
+		risita.heightInInches =5;
+		risita.haircolor ="black";
+		risita.speak();
+		
+	}
+
+}
--- Basics/src/com/basics/java/Human.java
+++ Basics/src/com/basics/java/Human.java
@@ -1,57 +1,20 @@
 package com.basics.java;
 
 public class Human {
-//prop
-String name="risita";
+String name;
 int age;
 int heightInInches;
-String hairColor;
-
-//behaviour
-public void speak() {
-	
-	System.out.println("abcd");
+String haircolor;
+public Human() {
 }
 
-public String getName() {
-	return name;
+ public void speak () {
+	 System.out.println("my name is "+ name);
+	 System.out.println("my age is "+ age+ " years");
+	 System.out.println(" my height is "+ heightInInches+" inches");
+	 System.out.println("my haircolor is "+ haircolor);
+	 
+ }
+
+
 }
-
-public void setName(String name) {
-	this.name = name;
-}
-
-public int getAge() {
-	return age;
-}
-
-public void setAge(int age) {
-	this.age = age;
-}
-
-public int getHeightInInches() {
-	return heightInInches;
-}
-
-public void setHeightInInches(int heightInInches) {
-	this.heightInInches = heightInInches;
-}
-
-public String getHairColor() {
-	return hairColor;
-}
-
-public void setHairColor(String hairColor) {
-	this.hairColor = hairColor;
-}
-}
-
-
-	
-
-
-
-
-
-
-
